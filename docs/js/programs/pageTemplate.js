93c31e6e19fe596fe1a05dd1669a9d69eea9bfdb
David McKnight <demcknight60@gmail.com>
2024-12-11 09:16:54 -0500

    setup proper export of program 25, 50 & 100 html files to SFR project v78

--- docs/js/programs/pageTemplate.js
+++ docs/js/programs/pageTemplate.js
@@ -44,44 +44,45 @@ export function initializePage(pageConfig) {
    }
 
    function updateTeamHeader(program) {
-       log(DEBUG_LEVELS.DEBUG, 'Updating team header', { team: program.Team });
-       const header = document.querySelector('.team-header');
-       if (!header) {
-           log(DEBUG_LEVELS.ERROR, 'Team header element not found');
-           return;
-       }
+    log(DEBUG_LEVELS.DEBUG, 'Updating team header', { team: program.team });
+    const header = document.querySelector('.team-header');
+    if (!header) {
+        log(DEBUG_LEVELS.ERROR, 'Team header element not found');
+        return;
+    }
 
-       const logoUrl = getImagePath(program.LogoURL);
-       const sealUrl = getImagePath(program.School_Logo_URL);
+    // Use the actual image paths from the program data
+    const logoUrl = program.LogoURL;  // This would come from WebV2
+    const schoolLogoUrl = program.School_Logo_URL;  // This would come from WebV2
 
-       header.innerHTML = `
-           <div class="container">
-               <div class="row align-items-center">
-                   <div class="col-md-3">
-                       <img src="${logoUrl}"
-                            alt="${program.Team} Logo"
-                            class="img-fluid"
-                            style="max-height: 100px;"
-                            onerror="this.src='${teamConfig.defaultLogo}'" />
-                   </div>
-                   <div class="col-md-6 text-center">
-                       <h2>${program.Team}</h2>
-                       <p>${program.Mascot || ''}</p>
-                   </div>
-                   <div class="col-md-3 text-right">
-                       <img src="${sealUrl}"
-                            alt="${program.Mascot}"
-                            class="img-fluid"
-                            style="max-height: 100px;"
-                            onerror="this.src='${teamConfig.defaultLogo}'" />
-                   </div>
-               </div>
-           </div>
-       `;
+    header.innerHTML = `
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-3">
+                    <img src="${logoUrl}"
+                         alt="${program.Team} Logo"
+                         class="img-fluid"
+                         style="max-height: 100px;"
+                         onerror="this.onerror=null; this.src='${teamConfig.defaultLogo}';" />
+                </div>
+                <div class="col-md-6 text-center">
+                    <h2>${program.Team}</h2>
+                    <p>${program.Mascot || ''}</p>
+                </div>
+                <div class="col-md-3 text-right">
+                    <img src="${schoolLogoUrl}"
+                         alt="${program.Mascot}"
+                         class="img-fluid"
+                         style="max-height: 100px;"
+                         onerror="this.onerror=null; this.src='${teamConfig.defaultLogo}';" />
+                </div>
+            </div>
+        </div>
+    `;
 
-       header.style.backgroundColor = program.PrimaryColor || '#000000';
-       header.style.color = program.SecondaryColor || '#FFFFFF';
-   }
+    header.style.backgroundColor = program.PrimaryColor || '#000000';
+    header.style.color = program.SecondaryColor || '#FFFFFF';
+}
 
    // ... rest of the functions (displayCurrentPage, handleSearch, etc.) ...
 
